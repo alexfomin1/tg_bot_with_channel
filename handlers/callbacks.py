0ef4b520728dc9cb7f956b5ccea717184171cf1f
Alex Fomin <me@fomin3.ru>
2022-08-16 10:03:19 +0300

    tg bot

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,7 +1,7 @@
 from aiogram import types
 from dispatcher import dp
 import config
-from decouple import config
+
 
 
 @dp.callback_query_handler(lambda c: c.data == 'get_link_button')
@@ -13,7 +13,7 @@ async def process_callback_get_link_button(c: types.CallbackQuery):
 			with open('link.txt', 'r') as f:
 				content = f.readlines()
 				f.close()
-				
+
 			await bot.answer_callback_query(c.id, 'Link - {}'.format(content[0].strip()), show_alert=True)
 		else:
 			await bot.answer_callback_query(c.id, 'First you need to subscribe to the channel!', show_alert=True)
